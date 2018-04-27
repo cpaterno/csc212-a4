2155f7af466c80dc9491b124fda0c0e770963c43
cpaterno <cpaterno@my.uri.edu>
2018-04-27 12:01:17 -0400

    Read command line argument, now have to read file

--- main.cc
+++ main.cc
@@ -1,4 +1,8 @@
 #include "board.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <cmath> 
 
 // -----------------------------------------------------------------------
 // -----------------------------------------------------------------------
@@ -18,6 +22,12 @@ void solve(const unsigned int *b, unsigned int n, char type) {
 // -----------------------------------------------------------------------
 int main(int argc, char **argv) {
     // reads the search type from a command line argument (either h or m)
+    if (argc != 2) return 1;
+    char type = argv[1][0];
+    bool firstNum = false;
+    int index = 0;
+    int num = 0;
+    int len = 0;
     // reads all initial board values from the stdin
     // calls the solver passing the values of the board and the search type
 }
