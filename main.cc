ed2de7b56fd505b0c6be257639936be70ab9e42a
Christopher Paterno <cpaterno@my.uri.edu>
2018-05-06 02:45:20 +0000

    Added back remove memory leak part

--- main.cc
+++ main.cc
@@ -41,32 +41,34 @@ void solve(unsigned int *b, unsigned int n, char type) {
     // initialize minheap priority queue and hash table
     std::priority_queue<Board*, std::vector<Board*>, Comparator> boardQ;
     std::unordered_set<std::string> visited;
-    // add the first board and its string representation into 
+    // add the first board and its string representation into
     // the correct data structures
     boardQ.emplace(&root);
     visited.insert(boardStr);
     // while the queue is not empty or base cases not hit
     // do A* search
     while(!boardQ.empty()) {
-    	// get the board with the lowest priority 
+    	// get the board with the lowest priority
     	// and remove it from the queue
     	goalBoard = boardQ.top();
     	boardQ.pop();
     	// base case 1: board is solved
     	if (goalBoard->is_goal()) {
     		std::cout << "Number of moves: " << goalBoard->get_n_moves();
+    		delete goalBoard;
     		break;
     	}
     	// base case 2: board is unsolvable
     	if (!goalBoard->is_solvable()) {
     		std::cout << "Unsolvable board";
-    		break; 
+    		delete goalBoard;
+    		break;
     	}
     	// create a vector of neighbors
     	std::vector<Board*> neigh;
     	goalBoard->neighbors(&neigh, type);
     	// check if each neighbor has never been seen before
-    	// and if not add it to the queue and hash table 
+    	// and if not add it to the queue and hash table
     	for(unsigned int i = 0; i < neigh.size(); i++) {
     		boardStr = neigh[i]->boardToStr();
     		// if the string representation of the board is not
@@ -77,6 +79,11 @@ void solve(unsigned int *b, unsigned int n, char type) {
     			visited.insert(boardStr);
     		}
     	}
+    }
+    while(!board.empty()) {
+        goalBoard = boardQ.top();
+        boardQ.pop();
+        delete boardQ;
     }
 }
 
@@ -97,7 +104,7 @@ int main(int argc, char **argv) {
     	std::cin >> input[i];
     }
     // calls the solver passing the values of the board and the search type
-    //solve(input, n, type);
-    testMethods(input, n, type);
+    solve(input, n, type);
+    //testMethods(input, n, type);
     return 0;
 }
